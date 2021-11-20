16acae9812b435ced1c5ebcc80c4b1d206bfb8fd
jacob swink <jacobswink1@yahoo.com>
2021-11-19 20:39:30 -0500

    Add Swagger Documentation to API
    Add Swagger to the API for Documentation and Testing

---
+++ TRMDataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,57 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace TRMDataManager.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            // Add a new route in swagger for log-in
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                // Post command for the path
+                post = new Operation
+                {
+                    // Place route into the Auth category
+                    tags = new List<string> { "Auth" },
+                    // Define the type of data that will be sent through the parameters
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    }
+                },
+                // Define the parameters
+                parameters = new List<Parameter>
+                {
+                    new Parameter
+                    {
+                        type = "string",
+                        name = "grant_type",
+                        required = true,
+                        @in = "formData",
+                        @default = "password"
+                    },
+                    new Parameter
+                    {
+                        type = "string",
+                        name = "username",
+                        required = false,
+                        @in = "formData"
+                    },
+                    new Parameter
+                    {
+                        type = "string",
+                        name = "password",
+                        required = false,
+                        @in = "formData"
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ TRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,33 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace TRMDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            // Add a parameter to every operation
+
+            // Initialize the parameters if there is none so we can use it
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            // Add an Authorization Token parameter to every operation
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
